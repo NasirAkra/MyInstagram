f975d2a185178259c4d7ebcea3730d9f25671794
Nasir Akram <paknasirakram@gmail.com>
2024-12-20 16:18:28 +0500

    Add fragments

---
+++ app/src/main/java/com/brain/myinstagramworld/fragments/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.brain.myinstagramworld.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.brain.myinstagramworld.R
+import com.brain.myinstagramworld.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding=FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+    }
+}
